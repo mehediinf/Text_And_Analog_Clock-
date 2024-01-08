19996d5c4b02841fcd5d7f86f4ac43b59404c516
mehedi-hasan <mehediinform@gmail.com>
2024-01-09 03:08:12 +0600

    2nd Commit Text_And_Analog_Clock

--- app/src/main/java/com/example/text_and_analog_clock/MainActivity.java
+++ app/src/main/java/com/example/text_and_analog_clock/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.text_and_analog_clock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.TextClock;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextClock textClock;
+    private AnalogClock analogClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        analogClock = findViewById(R.id.analogClock_Id);
+        textClock = findViewById(R.id.textClock_Id);
+
+        analogClock.setOnClickListener(this);
+        textClock.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId()==R.id.analogClock_Id){
+            Toast.makeText(MainActivity.this,"Analog Clock",Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId()==R.id.textClock_Id){
+            Toast.makeText(MainActivity.this,"Text Clock",Toast.LENGTH_SHORT).show();
+        }
     }
 }
