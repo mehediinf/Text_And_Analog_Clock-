2630f4a3d8a9237e91459574df7ff53803d08f35
mehedi-hasan <mehediinform@gmail.com>
2024-01-09 02:53:28 +0600

    1st Commit Text_And_Analog_Clock

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Text_And_Analog_Clock"
+include(":app")
+ 
